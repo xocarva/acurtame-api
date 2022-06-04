dd497d86231093325e1d0df189ae842997922727
xocarva <correo@xoancarneiro.com>
2022-06-04 16:43:49 +0200

    add database schema & initial data

---
+++ database/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO urls (url) VALUES("https://xoancarneiro.com");
+
+INSERT INTO urls (url) VALUES("https://library.xoancarneiro.com");
+
+INSERT INTO urls (url) VALUES("https://memoryram.xoancarneiro.com");
+
+INSERT INTO urls (url) VALUES("https://inquilinoperfecto.xoancarneiro.com");
